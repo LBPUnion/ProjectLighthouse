157258882c54324cffaaff0f6a4c7458ebbb2ebd
jvyden <jvyden@jvyden.xyz>
2021-10-05 23:02:13 -0400

    Initial Commit

---
+++ ProjectLighthouse/WeatherForecast.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ProjectLighthouse {
+    public class WeatherForecast {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
