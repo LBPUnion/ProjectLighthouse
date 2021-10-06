57aadb4eff08bdfb42cbfbb3c1cfe4d2db4e0dc4
jvyden <jvyden@jvyden.xyz>
2021-10-06 11:06:00 -0400

    Add constructor to Location

--- ProjectLighthouse/Types/Location.cs
+++ ProjectLighthouse/Types/Location.cs
@@ -2,12 +2,17 @@ using ProjectLighthouse.Serialization;
 
 namespace ProjectLighthouse.Types {
     public class Location {
+        public Location(int x, int y) {
+            this.X = x;
+            this.Y = y;
+        }
+        
         public int X;
         public int Y;
 
         public string Serialize() {
             return LbpSerializer.StringElement("x", this.X) +
-                   LbpSerializer.StringElement("Y", this.Y);
+                   LbpSerializer.StringElement("x", this.Y);
         }
     }
 }
