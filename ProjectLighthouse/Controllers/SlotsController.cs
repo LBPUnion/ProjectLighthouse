897f70928865b2b6b489349777e3f5bed61de9cd
jvyden <jvyden@jvyden.xyz>
2021-10-06 23:17:01 -0400

    Slots /s/user/id endpoint

--- ProjectLighthouse/Controllers/SlotsController.cs
+++ ProjectLighthouse/Controllers/SlotsController.cs
@@ -1,15 +1,28 @@
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProjectLighthouse.Serialization;
+using ProjectLighthouse.Types;
 
 namespace ProjectLighthouse.Controllers {
     [ApiController]
-    [Route("LITTLEBIGPLANETPS3_XML/slots")]
+    [Route("LITTLEBIGPLANETPS3_XML/")]
     [Produces("text/xml")]
     public class SlotsController : ControllerBase {
-        [HttpGet("by")]
+        [HttpGet("slots/by")]
         public IActionResult SlotsBy() {
-            return this.Ok(LbpSerializer.BlankElement("slots"));
+            string response = Enumerable.Aggregate(new Database().Slots, string.Empty, (current, slot) => current + slot.Serialize());
+
+            return this.Ok(LbpSerializer.TaggedStringElement("slots", response, "total", 1));
+        }
+
+        [HttpGet("s/user/{id:int}")]
+        public async Task<IActionResult> SUser(int id) {
+            Slot slot = await new Database().Slots.FirstOrDefaultAsync(s => s.SlotId == id);
+
+            return this.Ok(slot.Serialize());
         }
     }
 }
--- ProjectLighthouse/Types/Slot.cs
+++ ProjectLighthouse/Types/Slot.cs
@@ -2,6 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Xml.Serialization;
+using ProjectLighthouse.Serialization;
 
 namespace ProjectLighthouse.Types {
     /// <summary>
@@ -79,5 +80,26 @@ namespace ProjectLighthouse.Types {
         
         [XmlElement("moveRequired")]
         public bool MoveRequired { get; set; }
+
+        public string Serialize() {
+            string slotData = LbpSerializer.StringElement("name", Name) +
+                              LbpSerializer.StringElement("id", SlotId) +
+                              LbpSerializer.StringElement("game", 1) +
+                              LbpSerializer.StringElement("npHandle", "jvyden") +
+                              LbpSerializer.StringElement("description", Description) +
+                              LbpSerializer.StringElement("icon", IconHash) +
+                              LbpSerializer.StringElement("resource", Resource) +
+                              LbpSerializer.StringElement("location", Location.Serialize()) +
+                              LbpSerializer.StringElement("initiallyLocked", InitiallyLocked) +
+                              LbpSerializer.StringElement("isSubLevel", SubLevel) +
+                              LbpSerializer.StringElement("isLBP1Only", Lbp1Only) +
+                              LbpSerializer.StringElement("shareable", Shareable) +
+                              LbpSerializer.StringElement("background", BackgroundHash) +
+                              LbpSerializer.StringElement("minPlayers", MinimumPlayers) +
+                              LbpSerializer.StringElement("maxPlayers", MaximumPlayers) +
+                              LbpSerializer.StringElement("moveRequired", MoveRequired);
+            
+            return LbpSerializer.TaggedStringElement("slot", slotData, "type", "user");
+        }
     }
 }
