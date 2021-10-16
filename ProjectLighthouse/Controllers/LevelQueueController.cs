f0388732f33c47126f08273913cdb8df3ab9a18b
jvyden <jvyden@jvyden.xyz>
2021-10-16 17:39:18 -0400

    Add/Remove level queue endpoints

--- ProjectLighthouse/Controllers/LevelQueueController.cs
+++ ProjectLighthouse/Controllers/LevelQueueController.cs
@@ -1,6 +1,8 @@
+#nullable enable
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProjectLighthouse.Serialization;
@@ -22,6 +24,41 @@ namespace ProjectLighthouse.Controllers {
             string response = queuedLevels.Aggregate(string.Empty, (current, q) => current + q.Slot.Serialize());
 
             return this.Ok(LbpSerializer.TaggedStringElement("slots", response, "total", 1));
+        }
+
+        [HttpPost("lolcatftw/remove/user/{id:int}")]
+        public async Task<IActionResult> RemoveQueuedLevel(int id) {
+            await using Database database = new();
+
+            User? user = await database.UserFromRequest(this.Request);
+            if(user == null) return this.StatusCode(403, "");
+
+            QueuedLevel queuedLevel = await database.QueuedLevels.FirstOrDefaultAsync(q => q.UserId == user.UserId && q.SlotId == id);
+            if(queuedLevel != null) database.QueuedLevels.Remove(queuedLevel);
+
+            await database.SaveChangesAsync();
+
+            return this.Ok();
+        }
+
+        [HttpPost("lolcatftw/add/user/{id:int}")]
+        public async Task<IActionResult> AddQueuedLevel(int id) {
+            await using Database database = new();
+
+            User? user = await database.UserFromRequest(this.Request);
+            if(user == null) return this.StatusCode(403, "");
+
+            QueuedLevel queuedLevel = await database.QueuedLevels.FirstOrDefaultAsync(q => q.UserId == user.UserId && q.SlotId == id);
+            if(queuedLevel != null) return this.Ok();
+
+            database.QueuedLevels.Add(new QueuedLevel {
+                SlotId = id,
+                UserId = user.UserId
+            });
+
+            await database.SaveChangesAsync();
+
+            return this.Ok();
         }
     }
 }
