12177da47305376b5134fda36ba92e2938f4814a
jvyden <jvyden@jvyden.xyz>
2021-10-29 17:34:14 -0400

    Remove used slots from DB

---
+++ ProjectLighthouse/Migrations/20211029213334_RemoveUsedSlotsFromDb.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectLighthouse.Migrations
+{
+    public partial class RemoveUsedSlotsFromDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UsedSlots",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UsedSlots",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
