f4460264c3b123dc87beb779677c0c609e77d74d
jvyden <jvyden@jvyden.xyz>
2021-11-02 17:59:10 -0400

    Add support for team picks

---
+++ ProjectLighthouse/Migrations/20211102215859_RenameTeamPick.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectLighthouse.Migrations
+{
+    public partial class RenameTeamPick : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MMPick",
+                table: "Slots",
+                newName: "TeamPick");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TeamPick",
+                table: "Slots",
+                newName: "MMPick");
+        }
+    }
+}
