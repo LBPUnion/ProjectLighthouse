ce032d8e4d8c66cc96af769f9a6c0a63fba3d3bf
LumaLivy <livy@lumalivy.me>
2021-11-04 00:57:42 -0400

    Add another TaggedStringElement definition to support multiple key value pairs in XML tags

--- ProjectLighthouse/Serialization/LbpSerializer.cs
+++ ProjectLighthouse/Serialization/LbpSerializer.cs
@@ -26,6 +26,11 @@ namespace LBPUnion.ProjectLighthouse.Serialization
 
         public static string TaggedStringElement(string key, object value, string tagKey, object tagValue) => $"<{key} {tagKey}=\"{tagValue}\">{value}</{key}>";
 
+        public static string TaggedStringElement(string key, object value, Dictionary<string, object> attrKeyValuePairs)
+            => $"<{key} " +
+            Enumerable.Aggregate(attrKeyValuePairs, string.Empty, (current, kvp) => current + $"{kvp.Key}=\"{kvp.Value}\" ") +
+            $">{value}</{key}>";
+
         public static string Elements
             (params KeyValuePair<string, object>[] pairs)
             => pairs.Aggregate(string.Empty, (current, pair) => current + StringElement(pair));
