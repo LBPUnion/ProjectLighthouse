9dad2bce4d5dd568ce962f6e7012e5e5450eeb5e
LumaLivy <livy@lumalivy.me>
2021-11-08 05:15:31 -0500

    Migrations

---
+++ ProjectLighthouse/Migrations/20211108100949_GameSpecificPlayCountsRedo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectLighthouse.Migrations
+{
+    public partial class GameSpecificPlayCountsRedo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
