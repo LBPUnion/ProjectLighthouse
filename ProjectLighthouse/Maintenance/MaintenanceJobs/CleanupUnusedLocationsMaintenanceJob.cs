be01f95ac88a7f89f1b8c6bf378263ee07f19246
jvyden <jvyden@jvyden.xyz>
2021-11-24 18:51:22 -0500

    Log removed locations

--- ProjectLighthouse/Maintenance/MaintenanceJobs/CleanupUnusedLocationsMaintenanceJob.cs
+++ ProjectLighthouse/Maintenance/MaintenanceJobs/CleanupUnusedLocationsMaintenanceJob.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LBPUnion.ProjectLighthouse.Types.Profiles;
 
 namespace LBPUnion.ProjectLighthouse.Maintenance.MaintenanceJobs
 {
@@ -10,15 +12,22 @@ namespace LBPUnion.ProjectLighthouse.Maintenance.MaintenanceJobs
         public string Name() => "Cleanup Unused Locations";
         public string Description() => "Cleanup unused locations in the database.";
 
-        public Task Run()
+        public async Task Run()
         {
             List<int> usedLocationIds = new();
 
             usedLocationIds.AddRange(this.database.Slots.Select(slot => slot.LocationId));
             usedLocationIds.AddRange(this.database.Users.Select(user => user.LocationId));
 
-            this.database.RemoveRange(this.database.Locations.Where(l => !usedLocationIds.Contains(l.Id)));
-            return Task.CompletedTask;
+            IQueryable<Location> locationsToRemove = this.database.Locations.Where(l => !usedLocationIds.Contains(l.Id));
+
+            foreach (Location location in locationsToRemove)
+            {
+                Console.WriteLine("Removing location" + location.Id);
+                this.database.Locations.Remove(location);
+            }
+
+            await this.database.SaveChangesAsync();
         }
     }
 }
