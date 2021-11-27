f8d6c90e622a92ace0614425756aca73c00b9274
jvyden <jvyden@jvyden.xyz>
2021-11-27 15:34:59 -0500

    Add WipeTokensForUserCommand and DeleteAllTokensMaintenanceJob

---
+++ ProjectLighthouse/Maintenance/MaintenanceJobs/DeleteAllTokensMaintenanceJob.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Threading.Tasks;
+
+namespace LBPUnion.ProjectLighthouse.Maintenance.MaintenanceJobs
+{
+    public class DeleteAllTokensMaintenanceJob : IMaintenanceJob
+    {
+        private readonly Database database = new();
+
+        public string Name() => "Delete ALL Tokens";
+        public string Description() => "Deletes ALL game tokens and web tokens.";
+        public async Task Run()
+        {
+            this.database.GameTokens.RemoveRange(this.database.GameTokens);
+            this.database.WebTokens.RemoveRange(this.database.WebTokens);
+
+            await this.database.SaveChangesAsync();
+
+            Console.WriteLine("Deleted ALL tokens.");
+        }
+    }
+}
