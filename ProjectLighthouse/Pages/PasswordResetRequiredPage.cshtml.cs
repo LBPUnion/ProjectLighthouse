fa8e7a6b2423b841306dfa89e611231ce5fd127a
jvyden <jvyden@jvyden.xyz>
2021-11-27 19:45:05 -0500

    Add password reset page

--- ProjectLighthouse/Pages/PasswordResetRequiredPage.cshtml.cs
+++ ProjectLighthouse/Pages/PasswordResetRequiredPage.cshtml.cs
@@ -1,7 +1,6 @@
 #nullable enable
 using System.Threading.Tasks;
 using JetBrains.Annotations;
-using LBPUnion.ProjectLighthouse.Helpers;
 using LBPUnion.ProjectLighthouse.Pages.Layouts;
 using LBPUnion.ProjectLighthouse.Types;
 using Microsoft.AspNetCore.Mvc;
@@ -15,25 +14,11 @@ namespace LBPUnion.ProjectLighthouse.Pages
 
         public bool WasResetRequest { get; private set; }
 
-        public async Task<IActionResult> OnGet([FromQuery] string password, [FromQuery] string confirmPassword)
+        public async Task<IActionResult> OnGet()
         {
             User? user = this.Database.UserFromWebRequest(this.Request);
             if (user == null) return this.Redirect("~/login");
-            if (!user.PasswordResetRequired) return this.Redirect("~/resetPassword");
-
-            this.WasResetRequest = !string.IsNullOrEmpty(password) && !string.IsNullOrEmpty(confirmPassword);
-
-            if (this.WasResetRequest)
-            {
-                if (password != confirmPassword) return this.BadRequest();
-
-                user.Password = HashHelper.BCryptHash(password);
-                user.PasswordResetRequired = false;
-
-                await this.Database.SaveChangesAsync();
-
-                return this.Redirect("~/");
-            }
+            if (!user.PasswordResetRequired) return this.Redirect("~/passwordReset");
 
             return this.Page();
         }
