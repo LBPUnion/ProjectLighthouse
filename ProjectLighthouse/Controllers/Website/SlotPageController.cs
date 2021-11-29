45d7532292804b1cb9edf1980223f6302dc37976
jvyden <jvyden@jvyden.xyz>
2021-11-28 23:47:09 -0500

    Implement callback urls when hearting/queueing levels on the web

--- ProjectLighthouse/Controllers/Website/SlotPageController.cs
+++ ProjectLighthouse/Controllers/Website/SlotPageController.cs
@@ -5,6 +5,11 @@ using LBPUnion.ProjectLighthouse.Types.Levels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
+// I would like to apologize in advance for anyone dealing with this file. 
+// Theres probably a better way to do this with delegates but I'm tired.
+// TODO: Clean up this file
+// - jvyden
+
 namespace LBPUnion.ProjectLighthouse.Controllers.Website
 {
     [ApiController]
@@ -19,8 +24,13 @@ namespace LBPUnion.ProjectLighthouse.Controllers.Website
         }
 
         [HttpGet("heart")]
-        public async Task<IActionResult> HeartLevel([FromRoute] int id)
+        public async Task<IActionResult> HeartLevel([FromRoute] int id, [FromQuery] string? callbackUrl)
         {
+            if (string.IsNullOrEmpty(callbackUrl))
+            {
+                callbackUrl = "~/slot/" + id;
+            }
+
             User? user = this.database.UserFromWebRequest(this.Request);
             if (user == null) return this.Redirect("~/login");
 
@@ -29,12 +39,17 @@ namespace LBPUnion.ProjectLighthouse.Controllers.Website
 
             await this.database.HeartLevel(user, heartedSlot);
 
-            return this.Redirect("~/slot/" + id);
+            return this.Redirect(callbackUrl);
         }
 
         [HttpGet("unheart")]
-        public async Task<IActionResult> UnheartLevel([FromRoute] int id)
+        public async Task<IActionResult> UnheartLevel([FromRoute] int id, [FromQuery] string? callbackUrl)
         {
+            if (string.IsNullOrEmpty(callbackUrl))
+            {
+                callbackUrl = "~/slot/" + id;
+            }
+
             User? user = this.database.UserFromWebRequest(this.Request);
             if (user == null) return this.Redirect("~/login");
 
@@ -43,12 +58,17 @@ namespace LBPUnion.ProjectLighthouse.Controllers.Website
 
             await this.database.UnheartLevel(user, heartedSlot);
 
-            return this.Redirect("~/slot/" + id);
+            return this.Redirect(callbackUrl);
         }
 
         [HttpGet("queue")]
-        public async Task<IActionResult> QueueLevel([FromRoute] int id)
+        public async Task<IActionResult> QueueLevel([FromRoute] int id, [FromQuery] string? callbackUrl)
         {
+            if (string.IsNullOrEmpty(callbackUrl))
+            {
+                callbackUrl = "~/slot/" + id;
+            }
+
             User? user = this.database.UserFromWebRequest(this.Request);
             if (user == null) return this.Redirect("~/login");
 
@@ -57,12 +77,17 @@ namespace LBPUnion.ProjectLighthouse.Controllers.Website
 
             await this.database.QueueLevel(user, queuedSlot);
 
-            return this.Redirect("~/slot/" + id);
+            return this.Redirect(callbackUrl);
         }
 
         [HttpGet("unqueue")]
-        public async Task<IActionResult> UnqueueLevel([FromRoute] int id)
+        public async Task<IActionResult> UnqueueLevel([FromRoute] int id, [FromQuery] string? callbackUrl)
         {
+            if (string.IsNullOrEmpty(callbackUrl))
+            {
+                callbackUrl = "~/slot/" + id;
+            }
+
             User? user = this.database.UserFromWebRequest(this.Request);
             if (user == null) return this.Redirect("~/login");
 
@@ -71,7 +96,7 @@ namespace LBPUnion.ProjectLighthouse.Controllers.Website
 
             await this.database.UnqueueLevel(user, queuedSlot);
 
-            return this.Redirect("~/slot/" + id);
+            return this.Redirect(callbackUrl);
         }
     }
 }
