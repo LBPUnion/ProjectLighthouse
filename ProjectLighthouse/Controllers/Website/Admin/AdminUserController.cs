960f26b95c1edf352ebd0757b6208ee0bca1b827
jvyden <jvyden@jvyden.xyz>
2022-01-09 23:43:07 -0500

    Add ban confirmation page

--- ProjectLighthouse/Controllers/Website/Admin/AdminUserController.cs
+++ ProjectLighthouse/Controllers/Website/Admin/AdminUserController.cs
@@ -1,5 +1,4 @@
 #nullable enable
-using System.Linq;
 using System.Threading.Tasks;
 using LBPUnion.ProjectLighthouse.Types;
 using Microsoft.AspNetCore.Mvc;
@@ -18,34 +17,11 @@ namespace LBPUnion.ProjectLighthouse.Controllers.Website.Admin
             this.database = database;
         }
 
-        [HttpGet("ban")]
-        public async Task<IActionResult> BanUser([FromRoute] int id)
-        {
-            User? user = this.database.UserFromWebRequest(this.Request);
-            if (user == null || !user.IsAdmin) return this.StatusCode(403, "");
-
-            User? targetedUser = await this.database.Users.FirstOrDefaultAsync(u => u.UserId == id);
-            ;
-            if (targetedUser == null) return this.NotFound();
-
-            targetedUser.Banned = true;
-            targetedUser.BannedReason = $"Banned by admin {user.Username} (id: {user.UserId})";
-
-            // invalidate all currently active gametokens
-            this.database.GameTokens.RemoveRange(this.database.GameTokens.Where(t => t.UserId == targetedUser.UserId));
-
-            // invalidate all currently active webtokens
-            this.database.WebTokens.RemoveRange(this.database.WebTokens.Where(t => t.UserId == targetedUser.UserId));
-
-            await this.database.SaveChangesAsync();
-            return this.Redirect($"/user/{targetedUser.UserId}");
-        }
-
         [HttpGet("unban")]
         public async Task<IActionResult> UnbanUser([FromRoute] int id)
         {
             User? user = this.database.UserFromWebRequest(this.Request);
-            if (user == null || !user.IsAdmin) return this.StatusCode(403, "");
+            if (user == null || !user.IsAdmin) return this.NotFound();
 
             User? targetedUser = await this.database.Users.FirstOrDefaultAsync(u => u.UserId == id);
             ;
