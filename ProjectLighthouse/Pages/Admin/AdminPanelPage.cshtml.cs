ddd386370890c96f502c1d84e83cc8542bd7011f
jvyden <jvyden@jvyden.xyz>
2022-01-18 20:29:42 -0500

    Add users page to admin panel

--- ProjectLighthouse/Pages/Admin/AdminPanelPage.cshtml.cs
+++ ProjectLighthouse/Pages/Admin/AdminPanelPage.cshtml.cs
@@ -7,37 +7,35 @@ using LBPUnion.ProjectLighthouse.Pages.Layouts;
 using LBPUnion.ProjectLighthouse.Types;
 using Microsoft.AspNetCore.Mvc;
 
-namespace LBPUnion.ProjectLighthouse.Pages.Admin
+namespace LBPUnion.ProjectLighthouse.Pages.Admin;
+
+public class AdminPanelPage : BaseLayout
 {
-    public class AdminPanelPage : BaseLayout
+    public List<ICommand> Commands = MaintenanceHelper.Commands;
+    public AdminPanelPage(Database database) : base(database)
+    {}
+
+    public async Task<IActionResult> OnGet([FromQuery] string? args, [FromQuery] string? command, [FromQuery] string? maintenanceJob)
     {
+        User? user = this.Database.UserFromWebRequest(this.Request);
+        if (user == null) return this.Redirect("~/login");
+        if (!user.IsAdmin) return this.NotFound();
 
-        public List<ICommand> Commands = MaintenanceHelper.Commands;
-        public AdminPanelPage(Database database) : base(database)
-        {}
-
-        public async Task<IActionResult> OnGet([FromQuery] string? args, [FromQuery] string? command, [FromQuery] string? maintenanceJob)
+        if (!string.IsNullOrEmpty(command))
         {
-            User? user = this.Database.UserFromWebRequest(this.Request);
-            if (user == null) return this.Redirect("~/login");
-            if (!user.IsAdmin) return this.NotFound();
-
-            if (!string.IsNullOrEmpty(command))
-            {
-                args ??= "";
-                args = command + " " + args;
-                string[] split = args.Split(" ");
-                await MaintenanceHelper.RunCommand(split);
-                return this.Redirect("~/admin");
-            }
-
-            if (!string.IsNullOrEmpty(maintenanceJob))
-            {
-                await MaintenanceHelper.RunMaintenanceJob(maintenanceJob);
-                return this.Redirect("~/admin");
-            }
-
-            return this.Page();
+            args ??= "";
+            args = command + " " + args;
+            string[] split = args.Split(" ");
+            await MaintenanceHelper.RunCommand(split);
+            return this.Redirect("~/admin");
         }
+
+        if (!string.IsNullOrEmpty(maintenanceJob))
+        {
+            await MaintenanceHelper.RunMaintenanceJob(maintenanceJob);
+            return this.Redirect("~/admin");
+        }
+
+        return this.Page();
     }
 }
