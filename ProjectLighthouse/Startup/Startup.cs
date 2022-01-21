b1518400c425aaebd02f1384475090d7f349a1f5
jvyden <jvyden@jvyden.xyz>
2022-01-20 21:48:26 -0500

    Fix release building

--- ProjectLighthouse/Startup/Startup.cs
+++ ProjectLighthouse/Startup/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
 using Microsoft.Extensions.Primitives;
 
 namespace LBPUnion.ProjectLighthouse.Startup;
