e4fc9bf06c3db3663f4cc16f7905a3a867bea862
jvyden <jvyden@jvyden.xyz>
2022-03-09 17:22:20 -0500

    Log recent matches for individual games to influx

--- ProjectLighthouse/Helpers/InfluxHelper.cs
+++ ProjectLighthouse/Helpers/InfluxHelper.cs
@@ -1,9 +1,12 @@
+using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using InfluxDB.Client;
 using InfluxDB.Client.Writes;
 using Kettu;
 using LBPUnion.ProjectLighthouse.Logging;
+using LBPUnion.ProjectLighthouse.Types;
 using LBPUnion.ProjectLighthouse.Types.Settings;
 
 namespace LBPUnion.ProjectLighthouse.Helpers;
@@ -12,12 +15,30 @@ public static class InfluxHelper
 {
     public static readonly InfluxDBClient Client = InfluxDBClientFactory.Create(ServerSettings.Instance.InfluxUrl, ServerSettings.Instance.InfluxToken);
 
+    private static readonly List<GameVersion> gameVersions = new()
+    {
+        GameVersion.LittleBigPlanet1,
+        GameVersion.LittleBigPlanet2,
+        GameVersion.LittleBigPlanet3,
+        GameVersion.LittleBigPlanetVita,
+        GameVersion.LittleBigPlanetPSP,
+    };
+
     public static async void Log()
     {
         using WriteApi writeApi = Client.GetWriteApi();
         PointData point = PointData.Measurement("lighthouse")
             .Field("playerCount", await StatisticsHelper.RecentMatches())
             .Field("slotCount", await StatisticsHelper.SlotCount());
+
+        foreach (GameVersion gameVersion in gameVersions)
+        {
+            PointData gamePoint = PointData.Measurement("lighthouse")
+                .Tag("game", gameVersion.ToString())
+                .Field("playerCountGame", await StatisticsHelper.RecentMatchesForGame(gameVersion));
+
+            writeApi.WritePoint(ServerSettings.Instance.InfluxBucket, ServerSettings.Instance.InfluxOrg, gamePoint);
+        }
 
         writeApi.WritePoint(ServerSettings.Instance.InfluxBucket, ServerSettings.Instance.InfluxOrg, point);
 
@@ -34,10 +55,17 @@ public static class InfluxHelper
             {
                 while (true)
                 {
-                    #pragma warning disable CS4014
-                    Log();
-                    #pragma warning restore CS4014
-//                        Logger.Log("Logged.", LoggerLevelInflux.Instance);
+                    try
+                    {
+                        Log();
+                    }
+                    catch(Exception e)
+                    {
+                        Logger.Log("Exception while logging: ", LoggerLevelInflux.Instance);
+
+                        foreach (string line in e.ToString().Split("\n")) Logger.Log(line, LoggerLevelInflux.Instance);
+                    }
+
                     Thread.Sleep(60000);
                 }
             }
