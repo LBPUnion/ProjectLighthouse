5cabd5be101194f67c003ee1c412fed5a6b47ed4
jvyden <jvyden@jvyden.xyz>
2022-03-09 16:32:23 -0500

    Remove letters from CensorHelper._randomCharacters

--- ProjectLighthouse/Helpers/CensorHelper.cs
+++ ProjectLighthouse/Helpers/CensorHelper.cs
@@ -1,6 +1,5 @@
 using System.Text;
 using LBPUnion.ProjectLighthouse.Types.Settings;
-using Microsoft.Extensions.Primitives;
 
 namespace LBPUnion.ProjectLighthouse.Helpers;
 
@@ -16,59 +15,12 @@ public static class CensorHelper
 {
     private static readonly char[] _randomCharacters =
     {
-        '!',
-        '@',
-        '#',
-        '$',
-        '&',
-        '%',
-        '-',
-        'A',
-        'b',
-        'C',
-        'd',
-        'E',
-        'f',
-        'G',
-        'h',
-        'I',
-        'j',
-        'K',
-        'l',
-        'M',
-        'n',
-        'O',
-        'p',
-        'Q',
-        'r',
-        'S',
-        't',
-        'U',
-        'v',
-        'W',
-        'x',
-        'Y',
-        'z',
+        '!', '@', '#', '$', '&', '%', '-', '_',
     };
 
     private static readonly string[] _randomFurry =
     {
-        "UwU",
-        "OwO",
-        "uwu",
-        "owo",
-        "o3o",
-        ">.>",
-        "*pounces on you*",
-        "*boops*",
-        "*baps*",
-        ":P",
-        "x3",
-        "O_O",
-        "xD",
-        ":3",
-        ";3",
-        "^w^",
+        "UwU", "OwO", "uwu", "owo", "o3o", ">.>", "*pounces on you*", "*boops*", "*baps*", ":P", "x3", "O_O", "xD", ":3", ";3", "^w^",
     };
 
     private static readonly string[] _censorList = ResourceHelper.readManifestFile("chatCensoredList.txt").Split("\n");
@@ -83,10 +35,7 @@ public static class CensorHelper
             do
             {
                 profaneIndex = message.ToLower().IndexOf(profanity);
-                if (profaneIndex != -1)
-                    message = Censor(profaneIndex,
-                        profanity.Length,
-                        message);
+                if (profaneIndex != -1) message = Censor(profaneIndex, profanity.Length, message);
             }
             while (profaneIndex != -1);
 
@@ -101,14 +50,13 @@ public static class CensorHelper
         char randomChar;
         char prevRandomChar = '\0';
 
-        sb.Append(message.Substring(0,
-            profanityIndex));
+        sb.Append(message.Substring(0, profanityIndex));
 
         switch (ServerSettings.Instance.UserInputFilterMode)
         {
             case FilterMode.Random:
-                for (int i = 0; i < profanityLength; i++)
-                    lock (RandomHelper.random)
+                for(int i = 0; i < profanityLength; i++)
+                    lock(RandomHelper.random)
                     {
                         if (message[i] == ' ')
                         {
@@ -116,21 +64,18 @@ public static class CensorHelper
                         }
                         else
                         {
-                            randomChar = _randomCharacters[RandomHelper.random.Next(0,
-                                _randomCharacters.Length - 1)];
-                            if (randomChar == prevRandomChar)
-                                randomChar = _randomCharacters[RandomHelper.random.Next(0,
-                                    _randomCharacters.Length - 1)];
+                            randomChar = _randomCharacters[RandomHelper.random.Next(0, _randomCharacters.Length - 1)];
+                            if (randomChar == prevRandomChar) randomChar = _randomCharacters[RandomHelper.random.Next(0, _randomCharacters.Length - 1)];
 
                             prevRandomChar = randomChar;
 
-                            sb.Append(randomChar);   
+                            sb.Append(randomChar);
                         }
                     }
 
                 break;
             case FilterMode.Asterisks:
-                for (int i = 0; i < profanityLength; i++)
+                for(int i = 0; i < profanityLength; i++)
                 {
                     if (message[i] == ' ')
                     {
@@ -144,13 +89,12 @@ public static class CensorHelper
 
                 break;
             case FilterMode.Furry:
-                lock (RandomHelper.random)
+                lock(RandomHelper.random)
                 {
-                    randomWord = _randomFurry[RandomHelper.random.Next(0,
-                        _randomFurry.Length - 1)];
+                    randomWord = _randomFurry[RandomHelper.random.Next(0, _randomFurry.Length - 1)];
                     sb.Append(randomWord);
                 }
-                
+
                 break;
         }
 
