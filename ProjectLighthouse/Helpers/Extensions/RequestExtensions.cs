162da189d783f76fdc4216af2e6657f62fcfe474
jvyden <jvyden@jvyden.xyz>
2022-03-22 19:29:43 -0400

    Add PSVita to mobile check
    Closes #243

--- ProjectLighthouse/Helpers/Extensions/RequestExtensions.cs
+++ ProjectLighthouse/Helpers/Extensions/RequestExtensions.cs
@@ -8,13 +8,21 @@ using Microsoft.Net.Http.Headers;
 
 namespace LBPUnion.ProjectLighthouse.Helpers.Extensions;
 
-// yoinked and adapted from https://stackoverflow.com/a/68641796
 public static class RequestExtensions
 {
+    // yoinked and adapted from https://stackoverflow.com/a/68641796
+
+    #region Mobile Checking
+
     private static readonly Regex mobileCheck = new
-        ("Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini", RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.Compiled);
+    (
+        "Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|PlayStation Vita",
+        RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.Compiled
+    );
 
     public static bool IsMobile(this HttpRequest request) => mobileCheck.IsMatch(request.Headers[HeaderNames.UserAgent].ToString());
+
+    #endregion
 
     public static async Task<bool> CheckCaptchaValidity(this HttpRequest request)
     {
