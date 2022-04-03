213db4f2d84d13ba170e439a80ed0b591d87ac08
jvyden <jvyden@jvyden.xyz>
2022-04-03 19:07:36 -0400

    Add teampicks and trending levels on landing page
    Rough implementation for https://github.com/LBPUnion/ProjectLighthouse/issues/241

--- ProjectLighthouse/Pages/LandingPage.cshtml.cs
+++ ProjectLighthouse/Pages/LandingPage.cshtml.cs
@@ -6,20 +6,25 @@ using JetBrains.Annotations;
 using LBPUnion.ProjectLighthouse.Helpers;
 using LBPUnion.ProjectLighthouse.Pages.Layouts;
 using LBPUnion.ProjectLighthouse.Types;
+using LBPUnion.ProjectLighthouse.Types.Levels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.EntityFrameworkCore;
 
 namespace LBPUnion.ProjectLighthouse.Pages;
 
 public class LandingPage : BaseLayout
 {
+    public LandingPage(Database database) : base(database)
+    {}
 
     public int AuthenticationAttemptsCount;
     public List<User> PlayersOnline;
 
     public int PlayersOnlineCount;
-    public LandingPage(Database database) : base(database)
-    {}
+
+    public List<Slot> LatestTeamPicks;
+    public List<Slot> TrendingLevels;
 
     [UsedImplicitly]
     public async Task<IActionResult> OnGet()
@@ -37,6 +42,43 @@ public class LandingPage : BaseLayout
         List<int> userIds = await this.Database.LastContacts.Where(l => TimestampHelper.Timestamp - l.Timestamp < 300).Select(l => l.UserId).ToListAsync();
 
         this.PlayersOnline = await this.Database.Users.Where(u => userIds.Contains(u.UserId)).ToListAsync();
+
+        const int maxShownLevels = 5;
+
+        this.LatestTeamPicks = await this.Database.Slots.Where
+                (s => s.TeamPick)
+            .OrderBy(s => s.FirstUploaded)
+            .Take(maxShownLevels)
+            .Include(s => s.Creator)
+            .ToListAsync();
+
+        this.TrendingLevels = await this.Database.Slots.OrderByDescending
+                (s => s.PlaysLBP1Unique + s.PlaysLBP2Unique + s.PlaysLBP3Unique + s.PlaysLBPVitaUnique)
+            .ThenBy(s => s.FirstUploaded)
+            .Take(maxShownLevels)
+            .Include(s => s.Creator)
+            .ToListAsync();
+
         return this.Page();
     }
+
+    public ViewDataDictionary GetSlotViewData(int slotId, bool isMobile = false)
+        => new(ViewData)
+        {
+            {
+                "User", this.User
+            },
+            {
+                "CallbackUrl", $"~/slot/{slotId}"
+            },
+            {
+                "ShowLink", true
+            },
+            {
+                "IsMini", true
+            },
+            {
+                "IsMobile", isMobile
+            },
+        };
 }
