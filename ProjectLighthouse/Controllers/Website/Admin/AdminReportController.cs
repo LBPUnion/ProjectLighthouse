0298d5e69b32b58959b52d4fb2072aa44fd3dced
jvyden <jvyden@jvyden.xyz>
2022-04-04 16:05:18 -0400

    Several improvements to Grief Reports (#263)
    * Redesign reports page, add ability to dismiss reports
    
    * Delete report's attached jpeg when dismissing report

--- ProjectLighthouse/Controllers/Website/Admin/AdminReportController.cs
+++ ProjectLighthouse/Controllers/Website/Admin/AdminReportController.cs
@@ -52,4 +52,29 @@ public class AdminReportController : ControllerBase
 
         return this.Redirect("~/admin/reports/0");
     }
+
+    [HttpGet("dismiss")]
+    public async Task<IActionResult> DismissReport([FromRoute] int id)
+    {
+        User? user = this.database.UserFromWebRequest(this.Request);
+        if (user == null || !user.IsAdmin) return this.StatusCode(403, "");
+
+        GriefReport? report = await this.database.Reports.FirstOrDefaultAsync(r => r.ReportId == id);
+        if (report == null) return this.NotFound();
+
+        if (System.IO.File.Exists(Path.Combine("png", $"{report.JpegHash}.png")))
+        {
+            System.IO.File.Delete(Path.Combine("png", $"{report.JpegHash}.png"));
+        }
+        if (System.IO.File.Exists(Path.Combine("r", report.JpegHash)))
+        {
+            System.IO.File.Delete(Path.Combine("r", report.JpegHash));
+        }
+
+        this.database.Reports.Remove(report);
+
+        await this.database.SaveChangesAsync();
+
+        return this.Redirect("~/admin/reports/0");
+    }
 }
