1b48a71062d3ed88e20969bcbe4963930f217572
jvyden <jvyden@jvyden.xyz>
2022-04-14 16:50:30 -0400

    Add strings for entire baselayout

--- ProjectLighthouse.Localization/StringLists/BaseLayoutStrings.cs
+++ ProjectLighthouse.Localization/StringLists/BaseLayoutStrings.cs
@@ -6,6 +6,12 @@ public static class BaseLayoutStrings
     public static readonly TranslatableString HeaderUsers = create("header_users");
     public static readonly TranslatableString HeaderPhotos = create("header_photos");
     public static readonly TranslatableString HeaderSlots = create("header_slots");
+    public static readonly TranslatableString HeaderAuthentication = create("header_authentication");
+
+    public static readonly TranslatableString HeaderLoginRegister = create("header_loginRegister");
+    public static readonly TranslatableString HeaderProfile = create("header_profile");
+    public static readonly TranslatableString HeaderAdminPanel = create("header_adminPanel");
+    public static readonly TranslatableString HeaderLogout = create("header_logout");
 
     private static TranslatableString create(string key) => new(TranslationAreas.BaseLayout, key);
 }
