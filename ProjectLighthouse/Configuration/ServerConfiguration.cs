8ead63943f1a4db7d06d98b4a5ee8a77eb866bf4
jvyden <jvyden@jvyden.xyz>
2022-06-01 00:35:24 -0400

    Bump config version to 5

--- ProjectLighthouse/Configuration/ServerConfiguration.cs
+++ ProjectLighthouse/Configuration/ServerConfiguration.cs
@@ -23,7 +23,7 @@ public class ServerConfiguration
     // You can use an ObsoleteAttribute instead. Make sure you set it to error, though.
     //
     // Thanks for listening~
-    public const int CurrentConfigVersion = 4;
+    public const int CurrentConfigVersion = 5;
 
     #region Meta
 
