8a217a8d7910568270b4454124ebd3e0de638856
Henry Asbridge <henry@vorax.org>
2022-07-15 14:47:08 +0100

    Added user invite system

---
+++ ProjectLighthouse/PlayerData/APIKey.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LBPUnion.ProjectLighthouse.PlayerData
+{
+    public class APIKey
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public string Key { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public bool Enabled { get; set; }
+    }
+}
+
