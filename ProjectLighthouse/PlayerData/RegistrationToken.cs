ce0fe9edee355efde1a8e4eeb5650aecb5954110
Zaprit <henry@vorax.org>
2022-07-24 03:43:00 +0100

    Added user invite system (#351)
    * Added user invite system
    
    * Added user invite system
    
    * Revert recent migrations and try again
    
    * stopped implicitly assigning token variables
    
    * Added correct context to migrations
    
    * Apply suggestions from code review
    
    Some grammar changes, etc.
    
    Co-authored-by: Jayden <jvyden@jvyden.xyz>
    
    * Updated the API key page
    
    * Removed enabled field from APIKey
    
    * Removed reference to APIKey.Enabled
    
    * Add creation guide text
    
    * Fix this.Forbid() usage
    
    Causes an exception on my machine for some reason, always has.
    
    * Fix more forbid usages
    
    * Return 404 if trying to generate token when private registration is disabled
    
    * Capture authentication schema more cleanly
    
    Co-authored-by: Jayden <jvyden@jvyden.xyz>

---
+++ ProjectLighthouse/PlayerData/RegistrationToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LBPUnion.ProjectLighthouse.PlayerData
+{
+    public class RegistrationToken
+    {
+        [Key]
+        public int TokenId { get; set; }
+
+        public string Token { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}
+
