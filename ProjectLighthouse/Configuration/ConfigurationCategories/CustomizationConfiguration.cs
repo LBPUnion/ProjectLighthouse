7fbe145b807bfe73e1c14ea2b61cc020a28676a9
Toru the Red Fox <needforspeedfun@gmail.com>
2022-07-27 20:46:24 +0100

    Revert "remove numeric versioning for upstream merge"
    This reverts commit 3e7d8eccbc76283561b7b3cbd7f4f66697e6bcb6.

--- ProjectLighthouse/Configuration/ConfigurationCategories/CustomizationConfiguration.cs
+++ ProjectLighthouse/Configuration/ConfigurationCategories/CustomizationConfiguration.cs
@@ -4,4 +4,5 @@ public class CustomizationConfiguration
 {
     public string ServerName { get; set; } = "Project Lighthouse";
     public string EnvironmentName { get; set; } = "project-lighthouse";
+    public bool UseLessReliableNumericRevisionNumberingSystem { get; set; } = false;
 }
