ee11798dc6941f016ab80e4384d8fa014ede7e32
Josh <josh@slendy.pw>
2022-09-29 17:34:22 -0500

    Login with email and confirm age on registration (#493)
    * Implement login with email
    
    * Add confirm age checkbox to register page
    
    * Fix registration unit tests
    
    * Fix registration unit tests for real this time
    
    Co-authored-by: Dagg <32235163+daggintosh@users.noreply.github.com>

--- ProjectLighthouse.Localization/StringLists/GeneralStrings.cs
+++ ProjectLighthouse.Localization/StringLists/GeneralStrings.cs
@@ -4,6 +4,7 @@ public static class GeneralStrings
 {
     public static readonly TranslatableString Username = create("username");
     public static readonly TranslatableString Password = create("password");
+    public static readonly TranslatableString Email = create("email");
     public static readonly TranslatableString Register = create("register");
     public static readonly TranslatableString ResetPassword = create("reset_password");
     public static readonly TranslatableString ForgotPassword = create("forgot_password");
