eaeb9bdd1e1abfb63af44f20591f88d8b0b1b2fd
W0lf4llo <tyler0125@yahoo.com>
2022-11-05 03:27:25 -0400

    Added Lucky Dip to LBP3 & changed category desc

--- ProjectLighthouse/Levels/Categories/CategoryHelper.cs
+++ ProjectLighthouse/Levels/Categories/CategoryHelper.cs
@@ -12,6 +12,7 @@ public static class CategoryHelper
         Categories.Add(new NewestLevelsCategory());
         Categories.Add(new QueueCategory());
         Categories.Add(new HeartedCategory());
+        Categories.Add(new LuckyDipCategory());
 
         using Database database = new();
         foreach (DatabaseCategory category in database.CustomCategories) Categories.Add(new CustomCategory(category));
