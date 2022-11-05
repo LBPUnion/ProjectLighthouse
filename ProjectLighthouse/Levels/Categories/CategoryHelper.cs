d6fa1575466f5699d40516a2eb87fbd4bcf756c4
W0lf4llo <tyler0125@yahoo.com>
2022-11-05 19:13:41 -0400

    Removed "Most Hearted" as i havent made it yet

--- ProjectLighthouse/Levels/Categories/CategoryHelper.cs
+++ ProjectLighthouse/Levels/Categories/CategoryHelper.cs
@@ -13,7 +13,6 @@ public static class CategoryHelper
         Categories.Add(new QueueCategory());
         Categories.Add(new HeartedCategory());
         Categories.Add(new LuckyDipCategory());
-        Categories.Add(new MostHeartedCategory());
 
         using Database database = new();
         foreach (DatabaseCategory category in database.CustomCategories) Categories.Add(new CustomCategory(category));
