63b9941489e0a24e49ff6cdb1a2da03e3ce98975
W0lf4llo <tyler0125@yahoo.com>
2022-11-05 16:40:11 -0400

    Fixed the thing Slendy wanted me to fix lol

--- ProjectLighthouse/Levels/Categories/LuckyDipCategory.cs
+++ ProjectLighthouse/Levels/Categories/LuckyDipCategory.cs
@@ -13,7 +13,7 @@ public class LuckyDipCategory : Category
     public override string Name { get; set; } = "Lucky Dip";
     public override string Description { get; set; } = "Randomized uploaded content";
     public override string IconHash { get; set; } = "g820605";
-    public override string Endpoint { get; set; } = "luckydip";
+    public override string Endpoint { get; set; } = "lbp2luckydip";
     public override Slot? GetPreviewSlot(Database database) => database.Slots.Where(s => s.Type == SlotType.User).OrderByDescending(_ => EF.Functions.Random()).FirstOrDefault();
     public override IEnumerable<Slot> GetSlots
         (Database database, int pageStart, int pageSize)
