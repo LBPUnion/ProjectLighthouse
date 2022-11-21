674797662f16c5bfcd78aa4aa91fa39a0dde005a
W0lf4llo <96880844+W0lf4llo@users.noreply.github.com>
2022-11-21 00:15:45 -0500

    Update ProjectLighthouse.Servers.GameServer/Controllers/StatisticsController.cs
    Change 2
    
    Co-authored-by: Josh <josh@slendy.pw>

--- ProjectLighthouse.Servers.GameServer/Controllers/StatisticsController.cs
+++ ProjectLighthouse.Servers.GameServer/Controllers/StatisticsController.cs
@@ -39,9 +39,5 @@ public class StatisticsController : ControllerBase
     }
 
     [HttpGet("planetStats/totalLevelCount")]
-    public async Task<IActionResult> TotalLevelCount()
-    {
-        int a = await StatisticsHelper.SlotCountForGame(this.database, this.GetToken().GameVersion);
-        return this.Ok(a.ToString());
-    }
+    public async Task<IActionResult> TotalLevelCount() => this.Ok((await StatisticsHelper.SlotCountForGame(this.database, this.GetToken().GameVersion)).toString());
 }
