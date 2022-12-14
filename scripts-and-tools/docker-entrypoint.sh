16e6bf15e817f25741249323047347245a675b5f
Josh <josh@slendy.pw>
2022-12-13 20:06:32 -0600

    Run lighthouse as non root user

--- scripts-and-tools/docker-entrypoint.sh
+++ scripts-and-tools/docker-entrypoint.sh
@@ -6,10 +6,10 @@ if [ -d "/lighthouse/temp" ]; then
   cp -rf /lighthouse/temp/* /lighthouse/data
   rm -rf /lighthouse/temp
 fi
-su -s /bin/sh -l lighthouse
 
 # run from cmd
 
-cd /lighthouse/data && dotnet /lighthouse/app/LBPUnion.ProjectLighthouse.Servers."$SERVER".dll
+cd /lighthouse/data
+exec su-exec lighthouse:lighthouse dotnet /lighthouse/app/LBPUnion.ProjectLighthouse.Servers."$SERVER".dll
 
 exit $? # Expose error code from dotnet command
