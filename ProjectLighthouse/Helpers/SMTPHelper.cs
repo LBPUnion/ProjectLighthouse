86dff92179bacb014aca00a3de5b5a0e452d911b
Slendy <josh@slendy.pw>
2023-01-02 06:26:30 -0600

    Specify host and port for SmtpClient

--- ProjectLighthouse/Helpers/SMTPHelper.cs
+++ ProjectLighthouse/Helpers/SMTPHelper.cs
@@ -43,7 +43,7 @@ public class SMTPHelper
 
             try
             {
-                using SmtpClient client = new()
+                using SmtpClient client = new(ServerConfiguration.Instance.Mail.Host, ServerConfiguration.Instance.Mail.Port)
                 {
                     EnableSsl = ServerConfiguration.Instance.Mail.UseSSL,
                     Credentials = new NetworkCredential(ServerConfiguration.Instance.Mail.Username, ServerConfiguration.Instance.Mail.Password),
