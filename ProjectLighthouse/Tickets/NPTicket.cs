a49d3d9d2d5603f56ef723e9921bfcae1740a151
Slendy <josh@slendy.pw>
2023-01-02 03:18:45 -0600

    grr mondays

--- ProjectLighthouse/Tickets/NPTicket.cs
+++ ProjectLighthouse/Tickets/NPTicket.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using LBPUnion.ProjectLighthouse.Extensions;
 using LBPUnion.ProjectLighthouse.Helpers;
 using LBPUnion.ProjectLighthouse.Logging;
