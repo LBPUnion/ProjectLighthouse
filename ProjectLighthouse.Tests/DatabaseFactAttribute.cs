20b2ef5700c72c447a0a938eeded1632eb32b285
Josh <josh@slendy.pw>
2023-01-07 00:12:39 -0600

    Rework CI and add game server login tests (#615)
    * Make tests run in release mode
    
    * Fix multiple command in ci
    
    * I forgot how yaml works
    
    * Shitty workaround
    
    * grr mondays
    
    * Add an NP ticket builder for unit tests
    
    * Add NP ticket unit testing
    
    * Fix range indexers for finding uid
    
    * Fix LoginTests
    
    * Validate unit test signatures
    
    * Make builder code follow same style conventions
    
    * Remove remnant of hardcoded issuer id

--- ProjectLighthouse.Tests/DatabaseFactAttribute.cs
+++ ProjectLighthouse.Tests/DatabaseFactAttribute.cs
@@ -10,8 +10,10 @@ public sealed class DatabaseFactAttribute : FactAttribute
 
     public DatabaseFactAttribute()
     {
-        ServerConfiguration.Instance = new ServerConfiguration();
-        ServerConfiguration.Instance.DbConnectionString = "server=127.0.0.1;uid=root;pwd=lighthouse;database=lighthouse";
+        ServerConfiguration.Instance = new ServerConfiguration
+        {
+            DbConnectionString = "server=127.0.0.1;uid=root;pwd=lighthouse;database=lighthouse",
+        };
         if (!ServerStatics.DbConnected) this.Skip = "Database not available";
         else
             lock(migrateLock)
