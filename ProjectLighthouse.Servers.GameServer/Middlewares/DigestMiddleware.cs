3e977449a46fb11ad469a05110829fa4a84bcbbe
Toyo <87552280+Oo1Toyo1oO@users.noreply.github.com>
2023-01-14 19:50:09 +0100

    Sixt debug block test

--- ProjectLighthouse.Servers.GameServer/Middlewares/DigestMiddleware.cs
+++ ProjectLighthouse.Servers.GameServer/Middlewares/DigestMiddleware.cs
@@ -57,8 +57,13 @@ public class DigestMiddleware : Middleware
              }
              if (useragent == "MM CHTTPClient LBP2 01.26")
 {
+     
      GameToken? token = (GameToken?)(controller.HttpContext.Items["Token"] ?? null);
-     if (token.GameVersion == GameVersion.LittleBigPlanet2 || token == null) {
+if (token == null) {
+           context.Response.StatusCode = 403;
+            return;
+}
+     if (token.GameVersion == GameVersion.LittleBigPlanet2) {
             context.Response.StatusCode = 403;
             return;
      }
