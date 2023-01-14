ff17bc2b74cab08e7ea5f232e34756d3ae2fa2da
Toyo <87552280+Oo1Toyo1oO@users.noreply.github.com>
2023-01-14 22:32:08 +0100

    Blocking all Debug Builds

--- ProjectLighthouse.Servers.GameServer/Middlewares/DigestMiddleware.cs
+++ ProjectLighthouse.Servers.GameServer/Middlewares/DigestMiddleware.cs
@@ -6,6 +6,7 @@ using LBPUnion.ProjectLighthouse.Configuration;
 using LBPUnion.ProjectLighthouse.Helpers;
 using LBPUnion.ProjectLighthouse.Middlewares;
 using Microsoft.Extensions.Primitives;
+using Microsoft.Extensions.Primitives;
 
 namespace LBPUnion.ProjectLighthouse.Servers.GameServer.Middlewares;
 
@@ -32,7 +33,7 @@ public class DigestMiddleware : Middleware
     };
     #endif
 
-    public override async Task InvokeAsync(HttpContext context)
+    public override async Task InvokeAsync(HttpContext context, Database database)
     {
         // Client digest check.
         if (!context.Request.Cookies.TryGetValue("MM_AUTH", out string? authCookie))
@@ -53,6 +54,14 @@ public class DigestMiddleware : Middleware
                 if (useragent == "MM CHTTPClient $Id: HTTPClient.cpp 36247 2009-11-24 16:17:36Z paul $" || useragent == "MM CHTTPClient LBP2 01.00" || useragent == "MM CHTTPClient LBP2 01.02" || useragent == "MM CHTTPClient LBP2 01.03" || useragent == "MM CHTTPClient LBP2 01.04" || useragent == "MM CHTTPClient LBP2 01.05" || useragent == "MM CHTTPClient LBP2 01.06" || useragent == "MM CHTTPClient LBP2 01.07" || useragent == "MM CHTTPClient LBP2 01.08" || useragent == "MM CHTTPClient LBP2 01.09" || useragent == "MM CHTTPClient LBP2 01.10" || useragent == "MM CHTTPClient LBP2 01.11" || useragent == "MM CHTTPClient LBP2 01.12" || useragent == "MM CHTTPClient LBP2 01.13" || useragent == "MM CHTTPClient LBP2 01.14" || useragent == "MM CHTTPClient LBP2 01.15" || useragent == "MM CHTTPClient LBP2 01.16" || useragent == "MM CHTTPClient LBP2 01.17" || useragent == "MM CHTTPClient LBP2 01.18" || useragent == "MM CHTTPClient LBP2 01.19" || useragent == "MM CHTTPClient LBP2 01.20" || useragent == "MM CHTTPClient LBP2 01.21" || useragent == "MM CHTTPClient LBP2 01.22" || useragent == "MM CHTTPClient LBP2 01.23" || useragent == "MM CHTTPClient LBP2 01.24" || useragent == "MM CHTTPClient LBP2 01.25" || useragent == "MM CHTTPClient LBP2 01.27" || useragent == "MM CHTTPClient LBP2 01.28" || useragent == "MM CHTTPClient LBP2 01.29" || useragent == "MM CHTTPClient LBP2 01.30" || useragent == "MM CHTTPClient LBP2 01.31" || useragent == "MM CHTTPClient LBP2 01.32") {
                     context.Response.StatusCode = 403;
                     return;
+                }
+                else if (useragent == "MM CHTTPClient LBP2 01.26") {
+                    GameToken? gameToken = await database.GameTokenFromRequest(context.Request);
+                    if (gameToken == null) break;
+                    if (gameToken.GameVersion = GameVersion.LittleBigPlanet2) {
+                        context.Response.StatusCode = 403;
+                        return;
+                    }
                 }
              }
 
