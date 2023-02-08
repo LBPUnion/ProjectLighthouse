9a2513f20c4dd448b70dd158b7e02725cc061b26
Zaprit <henry@vorax.org>
2023-02-08 22:02:40 +0000

    Removed unnecessary  imports in UserPage.cshtml.cs

--- ProjectLighthouse.Servers.Website/Pages/UserPage.cshtml.cs
+++ ProjectLighthouse.Servers.Website/Pages/UserPage.cshtml.cs
@@ -4,7 +4,6 @@ using LBPUnion.ProjectLighthouse.Levels;
 using LBPUnion.ProjectLighthouse.PlayerData;
 using LBPUnion.ProjectLighthouse.PlayerData.Profiles;
 using LBPUnion.ProjectLighthouse.Servers.Website.Pages.Layouts;
-using LBPUnion.ProjectLighthouse.Types;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
