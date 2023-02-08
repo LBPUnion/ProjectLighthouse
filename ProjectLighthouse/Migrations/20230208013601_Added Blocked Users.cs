e85e5d7baa9d074dca051288b6c832b3d8eb005a
Zaprit <henry@vorax.org>
2023-02-08 16:23:51 +0000

    Fixed DB Migration

--- ProjectLighthouse/Migrations/20230208013601_Added Blocked Users.cs
+++ ProjectLighthouse/Migrations/20230208013601_Added Blocked Users.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using LBPUnion.ProjectLighthouse;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace ProjectLighthouse.Migrations
 {
+    [DbContext(typeof(Database))]
+    [Migration("20230208013601_Added Blocked Users")]
     /// <inheritdoc />
     public partial class AddedBlockedUsers : Migration
     {
