676e1672dedaeeed2f37dac2373ce60fa923da49
Zaprit <henry@vorax.org>
2023-02-08 22:02:59 +0000

    Made comments in-game respect blocked users

--- ProjectLighthouse.Servers.GameServer/Controllers/CommentController.cs
+++ ProjectLighthouse.Servers.GameServer/Controllers/CommentController.cs
@@ -71,9 +71,17 @@ public class CommentController : ControllerBase
 
         if (targetId == 0) return this.NotFound();
 
-        List<Comment> comments = await this.database.Comments.Include
-                (c => c.Poster)
-            .Where(c => c.TargetId == targetId && c.Type == type)
+        User? user = await this.database.UserFromGameToken(token);
+        
+        var blockedUsers = await (
+            from blockedProfile in this.database.BlockedProfiles
+            where blockedProfile.UserId == user.UserId
+            select blockedProfile.BlockedUserId
+        ).ToListAsync();
+        
+        List<Comment> comments = await this.database.Comments.Include(c => c.Poster)
+            .Where(c => c.TargetId == targetId && c.Type == type && !c.Poster.IsBanned)
+            .Where(c => !blockedUsers.Contains(c.PosterUserId))
             .OrderByDescending(c => c.Timestamp)
             .Skip(Math.Max(0, pageStart - 1))
             .Take(Math.Min(pageSize, 30))
