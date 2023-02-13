c8120b3388c58ccd40df1db942d3238238294dc8
Josh <josh@slendy.pw>
2023-02-12 23:37:26 -0600

    Remove InfluxDB and add /api/v1/playerCount (#664)
    * Add /playerCount api
    
    * Modify /playerCount api response
    
    * Remove all InfluxDB components

--- ProjectLighthouse.Servers.API/Controllers/StatisticsEndpoints.cs
+++ ProjectLighthouse.Servers.API/Controllers/StatisticsEndpoints.cs
@@ -1,6 +1,6 @@
 using LBPUnion.ProjectLighthouse.Helpers;
+using LBPUnion.ProjectLighthouse.PlayerData;
 using LBPUnion.ProjectLighthouse.Servers.API.Responses;
-using LBPUnion.ProjectLighthouse.Types;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LBPUnion.ProjectLighthouse.Servers.API.Controllers;
@@ -36,4 +36,39 @@ public class StatisticsEndpoints : ApiEndpointController
                 TeamPicks = await StatisticsHelper.TeamPickCount(this.database),
             }
         );
+
+    private static readonly List<GameVersion> gameVersions = new()
+    {
+        GameVersion.LittleBigPlanet1,
+        GameVersion.LittleBigPlanet2,
+        GameVersion.LittleBigPlanet3,
+        GameVersion.LittleBigPlanetVita,
+        GameVersion.LittleBigPlanetPSP,
+    };
+
+    /// <summary>
+    /// Get player counts for each individual title
+    /// </summary>
+    /// <returns>An instance of PlayerCountResponse</returns>
+    [HttpGet("playerCount")]
+    [ProducesResponseType(typeof(PlayerCountResponse), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetPlayerCounts()
+    {
+        List<PlayerCountObject> gameList = new();
+        foreach (GameVersion version in gameVersions)
+        {
+            gameList.Add(new PlayerCountObject
+            {
+                Game = version.ToString(),
+                PlayerCount = await StatisticsHelper.RecentMatchesForGame(this.database, version),
+            });
+        }
+        PlayerCountResponse response = new()
+        {
+            TotalPlayerCount = await StatisticsHelper.RecentMatches(this.database),
+            Games = gameList,
+        };
+
+        return this.Ok(response);
+    }
 }
---
+++ ProjectLighthouse.Servers.API/Responses/PlayerCountResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace LBPUnion.ProjectLighthouse.Servers.API.Responses;
+
+public class PlayerCountObject
+{
+    public string Game { get; set; } = "";
+    public int PlayerCount { get; set; }
+}
+
+public class PlayerCountResponse
+{
+    public int TotalPlayerCount { get; set; }
+    public List<PlayerCountObject> Games { get; set; } = new();
+}
