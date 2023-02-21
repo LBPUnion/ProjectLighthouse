2e2a6d7ce9dea0a5d7977ba828109cf5b43f3337
Slendy <josh@slendy.pw>
2023-02-21 16:52:44 -0600

    Revert "Use exactName for migration lock"
    This reverts commit 76cee6a3ff27c2d4d209c1a223adbc12885e5ad7.

--- ProjectLighthouse/StartupTasks.cs
+++ ProjectLighthouse/StartupTasks.cs
@@ -159,7 +159,7 @@ public static class StartupTasks
         Stopwatch totalStopwatch = Stopwatch.StartNew();
         Stopwatch stopwatch = Stopwatch.StartNew();
         Logger.Info("Migrating database...", LogArea.Database);
-        MySqlDistributedLock mutex = new("LighthouseMigration", ServerConfiguration.Instance.DbConnectionString, exactName: true);
+        MySqlDistributedLock mutex = new("LighthouseMigration", ServerConfiguration.Instance.DbConnectionString);
         await using (await mutex.AcquireAsync())
         {
             stopwatch.Stop();
