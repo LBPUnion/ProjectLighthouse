7d8092c41466d55a061fa641694dfa97621a617f
Slendy <josh@slendy.pw>
2023-02-23 00:02:55 -0600

    Add grace period for container startup and optimize startup

--- scripts-and-tools/docker-entrypoint.sh
+++ scripts-and-tools/docker-entrypoint.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
-chown -R lighthouse:lighthouse /lighthouse/data
+log() {
+    local type="$1"; shift
+    printf '%s [%s] [Entrypoint]: %s\n' "$(date -Iseconds)" "$type" "$*"
+}
+
+log Note "Entrypoint script for Lighthouse $SERVER started".
+
+if [ ! -d "/lighthouse/data" ]; then
+    log Note "Creating data directory"
+    mkdir -p "/lighthouse/data"
+    chown -R lighthouse:lighthouse /lighthouse/data
+fi
 
 if [ -d "/lighthouse/temp" ]; then
-  cp -rf /lighthouse/temp/* /lighthouse/data
-  rm -rf /lighthouse/temp
+    log Note "Copying temp directory to data"
+    cp -rf /lighthouse/temp/* /lighthouse/data
+    rm -rf /lighthouse/temp
 fi
 
 # run from cmd
 
+log Note "Startup tasks finished, starting $SERVER..."
 cd /lighthouse/data
 exec su-exec lighthouse:lighthouse dotnet /lighthouse/app/LBPUnion.ProjectLighthouse.Servers."$SERVER".dll
 
