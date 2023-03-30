36c7acd75b6ccffbf9fb63932af9edc79604ebe5
Slendy <josh@slendy.pw>
2023-03-30 10:47:03 -0500

    Make DbSets virtual so they can be overridden by tests

--- ProjectLighthouse/Database/DatabaseContext.cs
+++ ProjectLighthouse/Database/DatabaseContext.cs
@@ -15,51 +15,51 @@ public partial class DatabaseContext : DbContext
 
     #region Tokens
     // ReSharper disable once InconsistentNaming
-    public DbSet<ApiKeyEntity> APIKeys { get; set; }
-    public DbSet<EmailSetTokenEntity> EmailSetTokens { get; set; }
-    public DbSet<EmailVerificationTokenEntity> EmailVerificationTokens { get; set; }
-    public DbSet<GameTokenEntity> GameTokens { get; set; }
-    public DbSet<PasswordResetTokenEntity> PasswordResetTokens { get; set; }
-    public DbSet<RegistrationTokenEntity> RegistrationTokens { get; set; }
-    public DbSet<WebTokenEntity> WebTokens { get; set; }
+    public virtual DbSet<ApiKeyEntity> APIKeys { get; set; }
+    public virtual DbSet<EmailSetTokenEntity> EmailSetTokens { get; set; }
+    public virtual DbSet<EmailVerificationTokenEntity> EmailVerificationTokens { get; set; }
+    public virtual DbSet<GameTokenEntity> GameTokens { get; set; }
+    public virtual DbSet<PasswordResetTokenEntity> PasswordResetTokens { get; set; }
+    public virtual DbSet<RegistrationTokenEntity> RegistrationTokens { get; set; }
+    public virtual DbSet<WebTokenEntity> WebTokens { get; set; }
     #endregion
 
     #region Users
-    public DbSet<CommentEntity> Comments { get; set; }
-    public DbSet<LastContactEntity> LastContacts { get; set; }
-    public DbSet<PhotoEntity> Photos { get; set; }
-    public DbSet<PhotoSubjectEntity> PhotoSubjects { get; set; }
-    public DbSet<PlatformLinkAttemptEntity> PlatformLinkAttempts { get; set; }
-    public DbSet<UserEntity> Users { get; set; }
+    public virtual DbSet<CommentEntity> Comments { get; set; }
+    public virtual DbSet<LastContactEntity> LastContacts { get; set; }
+    public virtual DbSet<PhotoEntity> Photos { get; set; }
+    public virtual DbSet<PhotoSubjectEntity> PhotoSubjects { get; set; }
+    public virtual DbSet<PlatformLinkAttemptEntity> PlatformLinkAttempts { get; set; }
+    public virtual DbSet<UserEntity> Users { get; set; }
     #endregion
 
     #region Levels
-    public DbSet<DatabaseCategoryEntity> CustomCategories { get; set; }
-    public DbSet<PlaylistEntity> Playlists { get; set; }
-    public DbSet<ReviewEntity> Reviews { get; set; }
-    public DbSet<ScoreEntity> Scores { get; set; }
-    public DbSet<SlotEntity> Slots { get; set; }
+    public virtual DbSet<DatabaseCategoryEntity> CustomCategories { get; set; }
+    public virtual DbSet<PlaylistEntity> Playlists { get; set; }
+    public virtual DbSet<ReviewEntity> Reviews { get; set; }
+    public virtual DbSet<ScoreEntity> Scores { get; set; }
+    public virtual DbSet<SlotEntity> Slots { get; set; }
     #endregion
 
     #region Interactions
-    public DbSet<BlockedProfileEntity> BlockedProfiles { get; set; }
-    public DbSet<HeartedLevelEntity> HeartedLevels { get; set; }
-    public DbSet<HeartedPlaylistEntity> HeartedPlaylists { get; set; }
-    public DbSet<HeartedProfileEntity> HeartedProfiles { get; set; }
-    public DbSet<QueuedLevelEntity> QueuedLevels { get; set; }
-    public DbSet<RatedCommentEntity> RatedComments { get; set; }
-    public DbSet<RatedLevelEntity> RatedLevels { get; set; }
-    public DbSet<RatedReviewEntity> RatedReviews { get; set; }
-    public DbSet<VisitedLevelEntity> VisitedLevels { get; set; }
+    public virtual DbSet<BlockedProfileEntity> BlockedProfiles { get; set; }
+    public virtual DbSet<HeartedLevelEntity> HeartedLevels { get; set; }
+    public virtual DbSet<HeartedPlaylistEntity> HeartedPlaylists { get; set; }
+    public virtual DbSet<HeartedProfileEntity> HeartedProfiles { get; set; }
+    public virtual DbSet<QueuedLevelEntity> QueuedLevels { get; set; }
+    public virtual DbSet<RatedCommentEntity> RatedComments { get; set; }
+    public virtual DbSet<RatedLevelEntity> RatedLevels { get; set; }
+    public virtual DbSet<RatedReviewEntity> RatedReviews { get; set; }
+    public virtual DbSet<VisitedLevelEntity> VisitedLevels { get; set; }
     #endregion
 
     #region Moderation
-    public DbSet<ModerationCaseEntity> Cases { get; set; }
-    public DbSet<GriefReportEntity> Reports { get; set; }
+    public virtual DbSet<ModerationCaseEntity> Cases { get; set; }
+    public virtual DbSet<GriefReportEntity> Reports { get; set; }
     #endregion
 
     #region Misc
-    public DbSet<CompletedMigrationEntity> CompletedMigrations { get; set; }
+    public virtual DbSet<CompletedMigrationEntity> CompletedMigrations { get; set; }
     #endregion
 
     #endregion
