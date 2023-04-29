cf84778aa344430c71c82b8331248b740e7bd425
koko <koko@drones.gay>
2023-04-29 02:24:27 +0000

    Display "Login" only when registration is off, remove unused BaseLayoutStrings

--- ProjectLighthouse.Localization/StringLists/BaseLayoutStrings.cs
+++ ProjectLighthouse.Localization/StringLists/BaseLayoutStrings.cs
@@ -2,14 +2,13 @@ namespace LBPUnion.ProjectLighthouse.Localization.StringLists;
 
 public static class BaseLayoutStrings
 {
-    public static readonly TranslatableString HeaderHome = create("header_home");
     public static readonly TranslatableString HeaderUsers = create("header_users");
     public static readonly TranslatableString HeaderPhotos = create("header_photos");
     public static readonly TranslatableString HeaderSlots = create("header_slots");
     public static readonly TranslatableString HeaderAuthentication = create("header_authentication");
 
+    public static readonly TranslatableString HeaderLogin = create("header_login");
     public static readonly TranslatableString HeaderLoginRegister = create("header_loginRegister");
-    public static readonly TranslatableString HeaderProfile = create("header_profile");
     public static readonly TranslatableString HeaderAdminPanel = create("header_adminPanel");
     public static readonly TranslatableString HeaderModPanel = create("header_modPanel");
     public static readonly TranslatableString HeaderLogout = create("header_logout");
