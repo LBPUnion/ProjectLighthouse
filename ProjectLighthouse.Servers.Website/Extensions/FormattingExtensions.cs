3e727c9d8c5596004b5b07a3bc9ad23e1bb58a63
sudokoko <koko@drones.gay>
2023-05-14 01:45:44 -0400

    Extend slot badges to include team picked and potential warnings (i.e. lbp1 only)

--- ProjectLighthouse.Servers.Website/Extensions/FormattingExtensions.cs
+++ ProjectLighthouse.Servers.Website/Extensions/FormattingExtensions.cs
@@ -5,7 +5,21 @@ namespace LBPUnion.ProjectLighthouse.Servers.Website.Extensions;
 
 public static class FormattingExtensions
 {
-    public static string GetLevelLockIcon(this SlotEntity slot) => slot.InitiallyLocked ? "icon lock" : "";
+    public static string GetLevelLockIcon(this SlotEntity slot) => slot.InitiallyLocked ? "ui white icon lock" : "";
+    public static string GetTeamPickedIcon(this SlotEntity slot) => slot.TeamPick ? "ui pink icon certificate" : "";
+    
+    // ReSharper disable once UnusedParameter.Global
+    public static string GetLevelWarningIcon(this SlotEntity slot) => "ui orange icon exclamation circle";
+
+    // ReSharper disable once ConvertIfStatementToReturnStatement
+    // These messages are sorted by logical priority. No two should happen at once.
+    public static string GetLevelWarningText(this SlotEntity slot)
+    {
+        if (slot.Lbp1Only) return "This slot is designed for LittleBigPlanet 1 only.";
+        if (slot.CrossControllerRequired) return "This slot is designed to be played in Cross-Controller mode.";
+        if (slot.MoveRequired) return "This level requires a PlayStation Move controller.";
+        return "";
+    }
 
     public static string ToHtmlColor(this PermissionLevel permissionLevel)
     {
