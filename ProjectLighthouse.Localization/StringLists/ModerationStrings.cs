aa2b457b5907807c8f44e177ea4c6a3bc1e2e574
sudokoko <koko@drones.gay>
2023-05-26 13:27:36 -0400

    Add proper ban page upon logging in

---
+++ ProjectLighthouse.Localization/StringLists/ModerationStrings.cs
@@ -0,0 +1,16 @@
+﻿namespace LBPUnion.ProjectLighthouse.Localization.StringLists;
+
+public static class ModerationStrings
+{
+    public static readonly TranslatableString BannedHeading = create("banned_heading");
+    public static readonly TranslatableString BannedExplain = create("banned_explain");
+
+    public static readonly TranslatableString BanReason = create("ban_reason");
+
+    public static readonly TranslatableString CaseCreated = create("case_created");
+    public static readonly TranslatableString CaseExpires = create("case_expires");
+
+    public static readonly TranslatableString AppealInstructions = create("appeal_instructions");
+
+    private static TranslatableString create(string key) => new(TranslationAreas.Moderation, key);
+}
