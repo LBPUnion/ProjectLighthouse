212c38006f12e1b249b511a95a0cb033b4c8e251
sudokoko <koko@drones.gay>
2023-05-26 17:18:43 -0400

    Word choice nitpick ("Ban Created" -> "Ban Issued")

--- ProjectLighthouse.Localization/StringLists/ModerationStrings.cs
+++ ProjectLighthouse.Localization/StringLists/ModerationStrings.cs
@@ -5,7 +5,7 @@ public static class ModerationStrings
     public static readonly TranslatableString BanHeading = create("ban_heading");
     public static readonly TranslatableString BanExplanation = create("ban_explanation");
     public static readonly TranslatableString BanReason = create("ban_reason");
-    public static readonly TranslatableString BanCreated = create("ban_created");
+    public static readonly TranslatableString BanIssued = create("ban_issued");
     public static readonly TranslatableString BanExpires = create("ban_expires");
     public static readonly TranslatableString BanDoesNotExpire = create("ban_does_not_expire");
 
