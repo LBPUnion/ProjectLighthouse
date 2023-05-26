3483f067238ef58ce058a41a39709454b65b0821
sudokoko <koko@drones.gay>
2023-05-26 18:55:19 -0400

    Final adjustments and nitpicks, visual and grammatical

--- ProjectLighthouse.Localization/StringLists/ModerationStrings.cs
+++ ProjectLighthouse.Localization/StringLists/ModerationStrings.cs
@@ -2,12 +2,21 @@
 
 public static class ModerationStrings
 {
-    public static readonly TranslatableString BanHeading = create("ban_heading");
-    public static readonly TranslatableString BanExplanation = create("ban_explanation");
-    public static readonly TranslatableString BanReason = create("ban_reason");
-    public static readonly TranslatableString BanIssued = create("ban_issued");
-    public static readonly TranslatableString BanExpires = create("ban_expires");
-    public static readonly TranslatableString BanDoesNotExpire = create("ban_does_not_expire");
+    // Main page strings
+    public static readonly TranslatableString SuspensionHeading = create("suspension_heading");
+    public static readonly TranslatableString SuspensionExplanation = create("suspension_explanation");
+    public static readonly TranslatableString SuspensionExpiration = create("suspension_expiration");
+    public static readonly TranslatableString SuspensionReason = create("suspension_reason");
+    public static readonly TranslatableString SuspensionCircumventWarning = create("suspension_circumvent_warning");
+
+    // Translatable string in case a ban doesn't expire
+    public static readonly TranslatableString DoesNotExpire = create("does_not_expire");
+
+    // Restricted features strings
+    public static readonly TranslatableString LbpOnlineMultiplayer = create("lbp_online_multiplayer");
+    public static readonly TranslatableString WebsiteInteractions = create("website_interactions");
+    public static readonly TranslatableString ProfileVisibility = create("profile_visibility");
+    public static readonly TranslatableString AccountProfileManagement = create("account_profile_management");
 
     private static TranslatableString create(string key) => new(TranslationAreas.Moderation, key);
 }
