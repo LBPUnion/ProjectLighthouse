2f11731a8eef3c451c32acfbe364e105b1beed0e
Josh <josh@slendy.pw>
2023-06-07 00:16:29 -0500

    Refactor NpTicket parser and add NpTicket signature documentation (#784)
    * Refactor NpTicket parser and add NpTicket signature documentation
    
    * Update TickerBuilder chaining style
    
    * Fix SectionHeader position returning wrong value
    
    * Don't attempt to parse ticket if it is less than 8 bytes

---
+++ ProjectLighthouse/Tickets/Parser/TicketParseException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LBPUnion.ProjectLighthouse.Tickets.Parser;
+
+public class TicketParseException : Exception
+{
+    public TicketParseException(string message)
+    {
+        this.Message = message;
+    }
+
+    public override string Message { get; }
+}
---
+++ ProjectLighthouse/Tickets/Signature/NullSignatureVerifier.cs
@@ -0,0 +1,11 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+
+namespace LBPUnion.ProjectLighthouse.Tickets.Signature;
+
+public class NullSignatureVerifier : TicketSignatureVerifier
+{
+    protected override ECPublicKeyParameters PublicKey => null;
+    protected override string HashAlgorithm => null;
+    protected override bool VerifySignature(ISigner signer) => false;
+}
