d5b773926eb9ff645bc39ca749e74dfb8d8c0bba
sudokoko <koko@drones.gay>
2023-06-20 20:42:39 -0400

    Implement discord webhook support

--- ProjectLighthouse.Servers.Website/Pages/AnnouncePage.cshtml.cs
+++ ProjectLighthouse.Servers.Website/Pages/AnnouncePage.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿#nullable enable
 
+using LBPUnion.ProjectLighthouse.Configuration;
 using LBPUnion.ProjectLighthouse.Database;
+using LBPUnion.ProjectLighthouse.Helpers;
 using LBPUnion.ProjectLighthouse.Servers.Website.Pages.Layouts;
 using LBPUnion.ProjectLighthouse.Types.Entities.Profile;
 using LBPUnion.ProjectLighthouse.Types.Entities.Website;
@@ -46,6 +48,16 @@ public class AnnouncePage : BaseLayout
 
         await this.Database.WebsiteAnnouncements.AddAsync(announcement);
         await this.Database.SaveChangesAsync();
+
+        if (DiscordConfiguration.Instance.DiscordIntegrationEnabled)
+        {
+            string truncatedAnnouncement = content.Length > 100 ? content[..100] + "..." : content;
+            
+            await WebhookHelper.SendWebhook(
+                title: "A new announcement has been posted",
+                description: $"{truncatedAnnouncement} [read more]({ServerConfiguration.Instance.ExternalUrl}/announce)",
+                dest: WebhookHelper.WebhookDestination.Public);    
+        }
 
         return this.RedirectToPage();
     }
