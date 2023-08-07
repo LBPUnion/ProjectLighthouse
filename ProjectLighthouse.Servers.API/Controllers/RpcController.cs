5c54cd22e55de608a960a5336f90344201c112ec
koko <koko@drones.gay>
2023-08-07 09:56:09 -0400

    Implement basic RPC information endpoint with test data

---
+++ ProjectLighthouse.Servers.API/Controllers/RpcController.cs
@@ -0,0 +1,36 @@
+﻿using LBPUnion.ProjectLighthouse.Servers.API.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LBPUnion.ProjectLighthouse.Servers.API.Controllers;
+
+public class RpcController : ApiEndpointController
+{
+    /// <summary>
+    /// Returns basic information that RPC clients can use for self-configuration.
+    /// </summary>
+    /// <returns>RpcInformation</returns>
+    /// <response code="200">The RPC information.</response>
+    [HttpGet("rpc")]
+    [ProducesResponseType(typeof(RpcInformation), StatusCodes.Status200OK)]
+    public IActionResult GetRpc()
+    {
+        RpcInformation rpcInformation = new()
+        {
+            ApplicationId = 1060973475151495288,
+            PartyIdPrefix = "pl",
+            UsernameType = UsernameType.Integer,
+            Assets = new RpcAssets
+            {
+                PodAsset = "9c412649a07a8cb678a2a25214ed981001dd08ca",
+                MoonAsset = "a891bbcf9ad3518b80c210813cce8ed292ed4c62",
+                RemoteMoonAsset = "a891bbcf9ad3518b80c210813cce8ed292ed4c62",
+                DeveloperAsset = "7d3df5ce61ca90a80f600452cd3445b7a775d47e",
+                DeveloperAdventureAsset = "7d3df5ce61ca90a80f600452cd3445b7a775d47e",
+                DlcAsset = "2976e45d66b183f6d3242eaf01236d231766295f",
+                FallbackAsset = "e6bb64f5f280ce07fdcf4c63e25fa8296c73ec29",
+            },
+        };
+        
+        return this.Ok(rpcInformation);
+    }
+}
---
+++ ProjectLighthouse.Servers.API/Responses/RpcInformation.cs
@@ -0,0 +1,28 @@
+﻿#nullable disable
+
+namespace LBPUnion.ProjectLighthouse.Servers.API.Responses;
+
+public class RpcInformation
+{
+    public long ApplicationId { get; set; }
+    public string PartyIdPrefix { get; set; }
+    public UsernameType UsernameType { get; set; }
+    public RpcAssets Assets { get; set; }
+}
+
+public class RpcAssets
+{
+    public string PodAsset { get; set; }
+    public string MoonAsset { get; set; }
+    public string RemoteMoonAsset { get; set; }
+    public string DeveloperAsset { get; set; }
+    public string DeveloperAdventureAsset { get; set; }
+    public string DlcAsset { get; set; }
+    public string FallbackAsset { get; set; }
+}
+
+public enum UsernameType
+{
+    Integer = 0,
+    Username = 1,
+}
