d20b21e98ad02e3058342e28aac3e5594aa2efe7
Slendy <josh@slendy.pw>
2023-08-19 02:15:55 -0500

    Actually commit migration file for announcement timestamp

---
+++ ProjectLighthouse/Migrations/20230814031609_AddPublishedAtToAnnouncements.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using LBPUnion.ProjectLighthouse.Database;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectLighthouse.Migrations
+{
+    [DbContext(typeof(DatabaseContext))]
+    [Migration("20230814031609_AddPublishedAtToAnnouncements")]
+    public partial class AddPublishedAtToAnnouncements : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PublishedAt",
+                table: "WebsiteAnnouncements",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublishedAt",
+                table: "WebsiteAnnouncements");
+        }
+    }
+}
