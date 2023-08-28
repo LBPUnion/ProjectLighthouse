6c813127266c94cfa4387319f1b1cce4c5be282b
koko <koko@drones.gay>
2023-08-28 14:08:58 -0400

    Apply suggestions from code review

--- ProjectLighthouse.Servers.Website/Pages/UserSettingsPage.cshtml.cs
+++ ProjectLighthouse.Servers.Website/Pages/UserSettingsPage.cshtml.cs
@@ -20,7 +20,17 @@ public class UserSettingsPage : BaseLayout
     {}
 
     [SuppressMessage("ReSharper", "SpecifyStringComparison")]
-    public async Task<IActionResult> OnPost([FromRoute] int userId, [FromForm] string? avatar, [FromForm] string? username, [FromForm] string? email, [FromForm] string profileTag, [FromForm] string? biography, [FromForm] string? timeZone, [FromForm] string? language)
+    public async Task<IActionResult> OnPost
+    (
+        [FromRoute] int userId,
+        [FromForm] string? avatar,
+        [FromForm] string? username,
+        [FromForm] string? email,
+        [FromForm] string profileTag,
+        [FromForm] string? biography,
+        [FromForm] string? timeZone,
+        [FromForm] string? language
+    )
     {
         this.ProfileUser = await this.Database.Users.FirstOrDefaultAsync(u => u.UserId == userId);
         if (this.ProfileUser == null) return this.NotFound();
