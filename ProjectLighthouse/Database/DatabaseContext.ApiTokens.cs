a822645bcb505b290f9281fcfb013f278ff7f35f
koko <koko@drones.gay>
2023-09-21 20:52:52 -0400

    Move API authentication method into DatabaseContext partial class

---
+++ ProjectLighthouse/Database/DatabaseContext.ApiTokens.cs
@@ -0,0 +1,20 @@
+﻿#nullable enable
+using System.Linq;
+using LBPUnion.ProjectLighthouse.Types.Entities.Token;
+using Microsoft.AspNetCore.Http;
+
+namespace LBPUnion.ProjectLighthouse.Database;
+
+public partial class DatabaseContext
+{
+    public ApiKeyEntity? ApiKeyFromWebRequest(HttpRequest request)
+    {
+        string? authHeader = request.Headers["Authorization"];
+        if (string.IsNullOrWhiteSpace(authHeader)) return null;
+
+        string authToken = authHeader[(authHeader.IndexOf(' ') + 1)..];
+
+        ApiKeyEntity? apiKey = this.APIKeys.FirstOrDefault(k => k.Key == authToken);
+        return apiKey ?? null;
+    }
+}
