c1f94fad2303842712c2c5590397fc557adc2c2a
Slendy <josh@slendy.pw>
2023-10-25 13:17:50 -0500

    Remove vestigial code in VersionHelper

--- ProjectLighthouse/Helpers/VersionHelper.cs
+++ ProjectLighthouse/Helpers/VersionHelper.cs
@@ -1,42 +1,17 @@
-using System.Text.RegularExpressions;
 using LBPUnion.ProjectLighthouse.Configuration;
-using LBPUnion.ProjectLighthouse.Logging;
-using LBPUnion.ProjectLighthouse.Types.Logging;
 
 namespace LBPUnion.ProjectLighthouse.Helpers;
 
-public static partial class VersionHelper
+public static class VersionHelper
 {
-    static VersionHelper()
-    {
-        try
-        {
-            CommitHash = ThisAssembly.Git.Commit;
-            Branch = ThisAssembly.Git.Branch;
-            string commitNumber = $"{CommitHash}_{Build}";
-            FullRevision = Branch == "main" ? $"r{commitNumber}" : $"{Branch}_r{commitNumber}";
-        }
-        catch
-        {
-            Logger.Error("Project Lighthouse was built incorrectly. Please make sure git is available when building.",
-                LogArea.Startup);
-            CommitHash = "invalid";
-            Branch = "invalid";
-        }
+    public static string CommitHash => ThisAssembly.Git.Commit;
+    public static string Branch => ThisAssembly.Git.Branch;
 
-        if (!IsDirty) return;
-
-        Logger.Warn("This is a modified version of Project Lighthouse. " +
-                    "Please make sure you are properly disclosing the source code to any users who may be using this instance.",
-            LogArea.Startup);
-    }
-
-    public static string CommitHash { get; }
-    public static string Branch { get; }
     /// <summary>
     ///     The full revision string. States current revision hash and, if not main, the branch.
     /// </summary>
-    private static string FullRevision { get; set; }
+    private static string FullRevision => (Branch == "main" ? "" : $"{Branch}_") + $"r{CommitHash}_{Build}";
+
     /// <summary>
     ///     The server's branding (environment version) to show to LBP clients. Shows the environment name next to the
     ///     revision.
