9aecb85e3f9b4108a047a3c22c0fc3e629e6a2d6
Slendy <josh@slendy.pw>
2024-01-20 15:50:13 -0600

    Rename the model snapshot and move it to the actual migrations folder

--- ProjectLighthouse/Migrations/DatabaseContextModelSnapshot.cs
+++ ProjectLighthouse/Migrations/DatabaseContextModelSnapshot.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ProjectLighthouse.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseModelSnapshot : ModelSnapshot
+    partial class DatabaseContextModelSnapshot : ModelSnapshot
     {
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
