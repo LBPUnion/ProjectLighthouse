ed5bb5d76954d0fe88625b5f5b8e8ed73602191a
Josh <josh@slendy.pw>
2024-04-28 20:44:41 -0500

    Prevent LBP3 reviews from showing up in LBP2 (#1015)

--- ProjectLighthouse.Servers.GameServer/Controllers/Slots/ReviewController.cs
+++ ProjectLighthouse.Servers.GameServer/Controllers/Slots/ReviewController.cs
@@ -157,6 +157,13 @@ public class ReviewController : ControllerBase
 
         List<GameReview> reviews = (await this.database.Reviews
             .Where(r => r.SlotId == slotId)
+            .Select(r => new
+            {
+                Review = r,
+                SlotVersion = r.Slot!.GameVersion,
+            })
+            .Where(a => a.SlotVersion <= token.GameVersion)
+            .Select(a => a.Review)
             .OrderByDescending(r => r.ThumbsUp - r.ThumbsDown)
             .ThenByDescending(r => r.Timestamp)
             .ApplyPagination(pageData)
@@ -178,6 +185,13 @@ public class ReviewController : ControllerBase
 
         List<GameReview> reviews = (await this.database.Reviews
             .Where(r => r.ReviewerId == targetUserId)
+            .Select(r => new
+            {
+                Review = r,
+                SlotVersion = r.Slot!.GameVersion,
+            })
+            .Where(a => a.SlotVersion <= token.GameVersion)
+            .Select(a => a.Review)
             .OrderByDescending(r => r.Timestamp)
             .ApplyPagination(pageData)
             .ToListAsync()).ToSerializableList(r => GameReview.CreateFromEntity(r, token));
