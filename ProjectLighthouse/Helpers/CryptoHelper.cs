6ea846d002044b22f477734634b84e3855370b5f
Henry Asbridge <henry@vorax.org>
2024-06-16 01:27:01 +0100

    Update ProjectLighthouse/Helpers/CryptoHelper.cs
    Co-authored-by: Josh <josh@slendy.pw>

--- ProjectLighthouse/Helpers/CryptoHelper.cs
+++ ProjectLighthouse/Helpers/CryptoHelper.cs
@@ -19,7 +19,6 @@ public static class CryptoHelper
     public static string GenerateAuthToken()
     {
         byte[] bytes = (byte[])GenerateRandomBytes(256);
-        
         return Convert.ToBase64String(Encoding.UTF8.GetBytes(BCryptHash(Sha256Hash(bytes))));
     }
 
