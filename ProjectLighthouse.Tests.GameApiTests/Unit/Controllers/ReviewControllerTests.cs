573e7c4f1f565c168e0427f0cbb76319d5a9851f
Slendy <josh@slendy.pw>
2024-06-25 04:20:18 -0500

    Categorize review tests as unit tests

--- ProjectLighthouse.Tests.GameApiTests/Unit/Controllers/ReviewControllerTests.cs
+++ ProjectLighthouse.Tests.GameApiTests/Unit/Controllers/ReviewControllerTests.cs
@@ -13,6 +13,7 @@ using Xunit;
 
 namespace ProjectLighthouse.Tests.GameApiTests.Unit.Controllers;
 
+[Trait("Category", "Unit")]
 public class ReviewControllerTests
 {
     private static async Task InsertTestData(DatabaseContext database)
