967231603dfd8bd2160559761d286406e2ad6761
FeTetra <166051662+FeTetra@users.noreply.github.com>
2024-11-15 00:04:39 -0500

    Update ProjectLighthouse.Servers.Website/Pages/Debug/FilterTestPage.cshtml.cs
    Co-authored-by: sudokoko <sudo@koko.rip>

--- ProjectLighthouse.Servers.Website/Pages/Debug/FilterTestPage.cshtml.cs
+++ ProjectLighthouse.Servers.Website/Pages/Debug/FilterTestPage.cshtml.cs
@@ -19,7 +19,7 @@ public class FilterTestPage : BaseLayout
     public IActionResult OnGet(string? text = null)
     {
         #if DEBUG
-        if (text != null) this.FilteredText = CensorHelper.FilterMessage(text, "test filter", null);
+        if (text != null) this.FilteredText = CensorHelper.FilterMessage(text, "test filter");
         this.Text = text;
 
         return this.Page();
